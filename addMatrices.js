b15e3d48ecf1b2389b7115af4d8bcb909c3c5874
Rizwan Aman <rizwanaman5@gmail.com>
2019-10-08 20:30:17 +0530

    wrote a lot of comments on addition

--- addMatrices.js
+++ addMatrices.js
@@ -1,15 +1,34 @@
 // Write a program to add and subtract of given (NXN) Matrices
+/**
+ * matrice addition is quite simple on paper.
+ * we just add the elements of the corresponding matrices.
+ * lets see how we do that using algorithms
+ */
 
+ // these are the two matrices that are going to be added
 let matrice = [[1, 2], [3, 4], [5, 6]]
 let matrice2 = [[1, 2], [3, 4], [5, 6]]
+
+// This is the resulting matrice after the addition
 let result = []
 
+/**
+ * this is a function to calculate the dimensions of the matrice.
+ * did not prove to be usefull here, but maybe elsewhere.
+ * this function only checks the first element of the outer array.
+ * so it expects that the arrays within the arrays are or uniform length
+ */
 const dimensions = (matrice) => {
     console.log([matrice.length, matrice[0].length])
     return [matrice.length, matrice[0].length]
 }
 // dimensions(matrice)
 
+/**
+ * this is an important function, it verifies that the two matrices provided are of the same dimensions
+ * addition can only be performed on matrices of same dimensions.
+ * if they are different, the addition will still be performed but some elements will be NaN.
+ */
 const verifyMatrice = () => {
     let firstMatrice = {
         length: matrice.length,
@@ -23,6 +42,7 @@ const verifyMatrice = () => {
     }
     // console.log(secondMatrice);
 
+    // to compare two arrays, we need to use JSON.Stringify()
     if (firstMatrice.length == secondMatrice.length
         && JSON.stringify(firstMatrice.elementLength) == JSON.stringify(secondMatrice.elementLength)) {
 
@@ -35,6 +55,13 @@ const verifyMatrice = () => {
 }
 // verifyMatrice()
 
+/**
+ * this is the function which actually adds the matrices.
+ * .map can take three parameters, of which two are value and index
+ * so e and x are value and index and index2 are the indexes.
+ * we are pushing the result of e.map((x, index2) => x + matrice2[index][index2]) into the 
+ * result array declared above
+ */
 const addMatrice = () => {
     if (verifyMatrice()) {
         matrice.map((e, index) => {
