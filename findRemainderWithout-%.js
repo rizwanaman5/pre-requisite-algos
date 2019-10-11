cfc626a28e9b6c19dcda383809c02caff73a19e9
Rizwan Aman <rizwanaman5@gmail.com>
2019-10-11 12:44:27 +0530

    formsatting changes on file

--- findRemainderWithout-%.js
+++ findRemainderWithout-%.js
@@ -1,9 +1,9 @@
 // Write a program to find remainder of two numbers without using modulus (%) operator 
 // Dividing a by b...........
 function mod(a, b) {
-    if (b > a) {
-      console.log(a);
-    } else {
-      console.log(a - (b * Math.floor(a / b)));
-    }
+  if (b > a) {
+    console.log(a);
+  } else {
+    console.log(a - (b * Math.floor(a / b)));
   }
+}
